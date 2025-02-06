1c33885fddaa70285d08ec17d96b093c2b1ba9ff
Shruti <shrutipandeyofficial@gmail.com>
2025-02-06 16:18:47 +0530

    Mario game - Tight coupling

---
+++ learn-spring-framework/src/main/java/com/oreilly/spring/learn_spring_framework/game/MarioGame.java
@@ -0,0 +1,16 @@
+package com.oreilly.spring.learn_spring_framework.game;
+
+public class MarioGame {
+    public void up(){
+        System.out.println("Jump...");
+    }
+    public void down(){
+        System.out.println("Down into a hole.");
+    }
+    public void right(){
+        System.out.println("Accelerate++");
+    }
+    public void left(){
+        System.out.println("STOP");
+    }
+}
---
+++ learn-spring-framework/src/main/java/com/oreilly/spring/learn_spring_framework/game/SuperContraGame.java
@@ -0,0 +1,16 @@
+package com.oreilly.spring.learn_spring_framework.game;
+
+public class SuperContraGame {
+    public void up(){
+        System.out.println("SuperContra jump");
+    }
+    public void down(){
+        System.out.println("SuperContra down");
+    }
+    public void right(){
+        System.out.println("SuperContra right");
+    }
+    public void left(){
+        System.out.println("SuperContra left");
+    }
+}
